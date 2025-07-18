a945c11681d34e026ecef678e2f09b65a3844f5e
anshul <anshulsinghkamboj@gmail.com>
2025-07-18 12:26:54 +0530

    chunking complete

---
+++ data_chunking.py
@@ -0,0 +1,21 @@
+import re
+from langchain.docstore.document import Document
+
+def split_by_chapters(cleaned_text:str)->str:
+    """
+    split cleaned text based on chapter
+
+    """
+    parts=re.split(r'(CHAPTER\s[A-Z]+(?:\s[A-Z]+)*)',cleaned_text)
+
+    documents=[]
+
+    for i in range(1,len(parts),2):
+        title=parts[i].strip()
+        content=parts[i+1].strip()
+
+        doc=Document(page_content=content,
+                     metadata={"chapter_title": title ,"chapter":i//2+1})
+        documents.append(doc)
+
+    return documents
--- rag2.py
+++ rag2.py
@@ -1,8 +1,13 @@
 from data_ingestion import load_pdf_text
 from data_cleaning import clean_text
+from data_chunking import split_by_chapters
 from variables import PDF_FILE,DATA_FOLDER
 
-text = load_pdf_text(PDF_FILE)
-cleantext=clean_text(text)
 
-print(cleantext[:100])
+raw_text = load_pdf_text(PDF_FILE)
+cleaned_text = clean_text(raw_text)
+chapter_docs = split_by_chapters(cleaned_text)
+
+print(f"Extracted {len(chapter_docs)} chapters.")
+print(chapter_docs[0].metadata)
+print(chapter_docs[0].page_content[:500])
