4e70352e563037ba9abdbe120e8a9304f713ec12
anshul <anshulsinghkamboj@gmail.com>
2025-07-22 10:39:21 +0530

    changes

--- variables.py
+++ variables.py
@@ -1,4 +1,4 @@
 PDF_FILE='harry1.pdf'
 DATA_FOLDER='data'
-VECTORSTORE_PATH = "vectorstore/chapters_index"
-SUMMARY_STORE_PATH = "vectorstore/summaries_index"
+VECTORSTORE_PATH = "vectorstore_data/chapters_index"
+SUMMARY_STORE_PATH = "vectorstore_data/summaries_index"
